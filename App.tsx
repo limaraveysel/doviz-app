2ac99f752080c940db467786f3ec9bde95369b0f
Limara Veysel <limaraveysel@gmail.com>
2026-05-05 13:18:38 +0300

    update
    05.05.2026 13:18 update

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import {useState} from 'react';
 import {useEffect} from 'react';
 import { Picker} from '@react-native-picker/picker';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const API_KEY = '0f45916f7aff8ba3e74d0bc6';
 
@@ -11,14 +12,23 @@ export default function App(){
   const [amount, setAmount] = useState("1");
   const [from, setFrom] = useState("TRY");
   const [to, setTo] = useState("USD");
-  const [rates, setRates] = useState<{ [key: string]: number }>({});
+  const [rates, setRates] = useState<Record<string, number>>({});
   const [result, setResult] = useState<string>("0");
+  const [currencies, setCurrencies] =useState<string[]>([]);
+  const [history, setHistory] = useState<Record<string, any>>({});
+
+  //doviz kurlarini apiden cekme
  const getRates = async () => {
     try {
       const res = await axios.get(
         `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`
       );
-      setRates(res.data.conversion_rates);
+
+      const data = res.data.conversion_rates;
+
+      setRates(data);
+      setCurrencies(Object.keys(data)); //tum para birimleri al kaydet
+
     } catch (err) {
       console.log(err);
     }
@@ -36,6 +46,38 @@ export default function App(){
     }
   }, [amount, from, to, rates]);
 
+  //son yedı gunun kuru
+const getHistory = async () => {
+  try {
+    const today = new Date();
+    const lastWeek = new Date();
+    lastWeek.setDate(today.getDate() - 7);
+
+    const format = (d: Date) => d.toISOString().split("T")[0];
+
+    const res = await axios.get(
+      `https://api.frankfurter.app/${format(lastWeek)}..${format(today)}?from=${from}&to=${to}`
+    );
+
+    const rates = res.data?.rates;
+
+    if (!rates || typeof rates !== "object") {
+      setHistory({});
+      return;
+    }
+
+    setHistory(rates);
+  } catch (err) {
+    console.log("history error:", err);
+    setHistory({});
+  }
+};
+
+useEffect(() => {
+  getHistory();
+}, [from, to]);
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Döviz Çevirici</Text>
@@ -49,22 +91,60 @@ export default function App(){
 
       <View style={styles.row}>
         <Picker selectedValue={from} onValueChange={setFrom} style={styles.picker}>
-          <Picker.Item label="USD" value="USD" />
-          <Picker.Item label="TRY" value="TRY" />
-          <Picker.Item label="EUR" value="EUR" />
+              {currencies.map((curr) => (
+            <Picker.Item key={curr} label={curr} value={curr} />
+          ))}
         </Picker>
-
+        <View style={{ alignItems: "center", marginVertical: 10 }}>
+           <Icon
+                  name="exchange"
+                  size={40}
+                  color="#fff"
+                  onPress={() => {
+                  const temp = from;
+                  setFrom(to);
+                  setTo(temp);
+                }}
+             />
+         </View>
         <Picker selectedValue={to} onValueChange={setTo} style={styles.picker}>
-          <Picker.Item label="TRY" value="TRY" />
-          <Picker.Item label="USD" value="USD" />
-          <Picker.Item label="EUR" value="EUR" />
+          {currencies.map((curr) => (
+            <Picker.Item key={curr} label={curr} value={curr} />
+          ))}
         </Picker>
       </View>
 
       <Text style={styles.result}>
         Sonuç: {result} {to}
       </Text>
+
+<View style={{ marginTop: 20 }}>
+  {history &&
+    Object.keys(history).length > 0 &&
+    Object.entries(history).map(([date, value]: any) => (
+      <View
+        key={date}
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          padding: 10,
+          backgroundColor: "#1c1c1c",
+          marginBottom: 5,
+          borderRadius: 8,
+        }}
+      >
+        <Text style={{ color: "#fff" }}>
+          {date}
+        </Text>
+
+        <Text style={{ color: "#00f2fe" }}>
+          {value?.[to] ?? "N/A"}
+        </Text>
+      </View>
+    ))}
+</View>
     </View>
+    
   );
 }
 
@@ -73,7 +153,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     padding: 20,
-    backgroundColor: "#141E30",
+    backgroundColor: "#024c17",
   },
   title: {
     fontSize: 26,
